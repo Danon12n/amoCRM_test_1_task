38b626f441b3a000b172b6101210a0315d2f7f66
Danon12n <masterwars22@yandex.ru>
2023-04-28 20:22:49 +0300

    upd
    fixed mobile view

---
+++ src/App.js
@@ -0,0 +1,13 @@
+function App() {
+    return (
+        <div className='App'>
+            <input placeholder='Seconds' type='text' />
+            <button>Start</button>
+            <br />
+            <br />
+            <span>hh:mm:ss</span>
+        </div>
+    );
+}
+
+export default App;
